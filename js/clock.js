ff82cec0141373ccc0268d36cc71b647938f48f9
joshua.cook <joshua.cook@webappuk.com>
2016-09-23 16:03:47 +0100

    Made auto updating clock, initially to design new web page and REM calculator for clock.

---
+++ js/clock.js
@@ -0,0 +1,14 @@
+setInterval(()=>{
+    var timeObj = new Date();
+    time = {
+        hours : checkTime(timeObj.getHours()),
+        minutes : checkTime(timeObj.getMinutes()),
+        seconds : checkTime(timeObj.getSeconds())
+    }
+    $("#clockDiv").html(time.hours + ":" + time.minutes + ":" + time.seconds);
+},  10);
+
+//check time and add zero infront of any minute/second that is less than 10
+checkTime = (data)=>{
+    return (data < 10 ? "0" + data : data);
+}
